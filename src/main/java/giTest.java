ca6dc00802fc7a77f1a827d6427a5e94d218e040
Sun Heting <sunheting11@163.com>
2023-04-24 11:53:46 +0800

    hi git4

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -4,5 +4,8 @@ public class giTest {
         System.out.println("hellohhh");
         System.out.println("hellohhh");
         System.out.println("hellohhh");
+        System.out.println("rrrrr");
+        System.out.println("rrrrr");
+        System.out.println("rrrrr");
     }
 }
