0ee77b4506260e150aaddb91a636fa80eb08f0ca
sunheting <109885810+sunheting@users.noreply.github.com>
2023-04-24 15:06:08 +0800

    pull commit

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -9,5 +9,6 @@ public class giTest {
         System.out.println("master");
         System.out.println("hot fix");
         System.out.println("git push");
+        System.out.println("git pull");
     }
 }
