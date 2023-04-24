3efb1952f74c6e8b74ca750f9be4cdea241de605
Sun Heting <sunheting11@163.com>
2023-04-24 10:19:26 +0800

    hi git2

--- src/main/java/giTest.java
+++ src/main/java/giTest.java
@@ -1,5 +1,6 @@
 public class giTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellohhh");
+        System.out.println("hellohhh");
     }
 }
